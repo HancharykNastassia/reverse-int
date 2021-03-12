2d8ddc3fefe41bdd5457ee3616ff461567df7d16
HancharykNastassia <goncharik.anastasia@mail.ru>
2021-03-12 11:57:30 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  let numbers = [];
+  let num = n > 0 ? n : -n;
+  while (num > 9){
+    numbers.push(num % 10);
+    num = Math.floor(num / 10);
+  }
+  numbers.push(num);
+  let res = 0;
+  numbers.forEach(item => res = res*10 + item);
+  return res;
 }
